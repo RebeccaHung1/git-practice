59ddacfcd0c70741ab842484c98d44dd5ce9d752
Rebecca Hung <arisejlover@gmail.com>
2022-05-14 11:10:47 +0800

    test push

---
+++ 1-6.py
@@ -0,0 +1,15 @@
+a = input()
+a = int(a)
+
+if a % 4 != 0:
+    print("False")
+elif a % 4 == 0 and a % 100 != 0:
+    print("True")
+elif a % 100 == 0 and a % 400 != 0:
+    print("False")
+elif a % 400 == 0:
+    print("True")
+else:
+    print("False")
+# now we would like to try the push
+
